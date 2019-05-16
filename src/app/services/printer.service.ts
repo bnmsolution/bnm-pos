675988159cf58739e3160e338ca3afedff383b17
Bill Kim <bill.jh.kim@gmail.com>
2019-05-16 10:30:01 -0400

    wip commit

--- src/app/services/printer.service.ts
+++ src/app/services/printer.service.ts
@@ -4,43 +4,48 @@ import * as stringWidth from 'string-width';
 
 import { AppState } from '../core';
 import { format } from 'date-fns';
+import { HttpClient } from '@angular/common/http';
 
 
 @Injectable()
 export class PrinterService {
 
   constructor(
+    private http: HttpClient,
     private appState: AppState) { }
 
   sendPrintRequest(sale: RegisterSale) {
-    const store = this.appState.currentStore;
-    const header1 = `${store.name} ${store.phoneNumber}\\n`;
-    const header2 = `${store.address}\\n`;
-    const header3 = `${store.representative} ${store.businessNumber}\\n\\n`;
-    const header4 = format(new Date(sale.salesDate), 'yyyy-MM-dd HH:mm:ss') + '\\n';
-    const header5 = `${this.padString('상품명', 20, false)} ${this.padString('단가', 7)} ${this.padString('수량', 5)} ${this.padString('금액', 7)}\\n`;
+    const store = this.appState.currentStore as any;
+    const header1 = `${store.name} ${store.phoneNumber}`;
+    const header2 = `${store.address}`;
+    const header3 = `${store.representative} ${store.businessNumber}`;
+    const header4 = format(new Date(sale.salesDate), 'yyyy-MM-dd HH:mm:ss');
+    const header5 = `${this.padString(' 상 품 명', 19, false)} ${this.padString('단 가', 8)} ${this.padString('수량', 4)} ${this.padString('금 액', 8)}`;
     const data = [
       header1,
       header2,
       header3,
+      '',
       header4,
+      '-'.repeat(42) + '',
       header5,
-      '-'.repeat(42) + '\\n'];
+      '-'.repeat(42) + ''];
     sale.lineItems.forEach(li => {
-      const name = this.padString(li.name, 20, false);
-      const price = this.padString(this.formatNumber(li.finalPrice), 7);
+      const name = this.padString(li.name, 19, false);
+      const price = this.padString(this.formatNumber(li.finalPrice), 8);
       const qty = this.padString(this.formatNumber(li.quantity), 4);
-      const total = this.padString(this.formatNumber(li.finalTotal), 7);
-      data.push(`${name} ${price} ${qty} ${total}\\n`);
-      li.addons.forEach(ao => data.push(`\\u2515${this.padString(ao.name, 19, false)} ${this.padString(this.formatNumber(ao.price), 7)}\\n`));
+      const total = this.padString(this.formatNumber(li.finalTotal), 8);
+      data.push(`${name} ${price} ${qty} ${total}`);
+      li.addons.forEach(ao => data.push(` ㄴ${this.padString(ao.name, 17, false)} ${this.padString(this.formatNumber(ao.price), 7)}`));
     });
 
-    data.push('-'.repeat(42) + '\\n');
-    data.push(`${this.padString('과세물품가액', 31, false)} ${this.padString(this.formatNumber(sale.totalTaxablePrice), 10)}\\n`);
-    data.push(`${this.padString('부가세', 31, false)} ${this.padString(this.formatNumber(sale.totalTax), 10)}\\n`);
-    data.push(`${this.padString('합계', 31, false)} ${this.padString(this.formatNumber(sale.totalPrice), 10)}\\n`);
-
+    data.push('-'.repeat(42) + '');
+       data.push(`${this.padString('과세 물품가액', 31, false)} ${this.padString(this.formatNumber(sale.totalTaxablePrice), 10)}`);
+    data.push(`${this.padString('부가세', 31, false)} ${this.padString(this.formatNumber(sale.totalTax), 10)}`);
+    data.push(`${this.padString('합계', 31, false)} ${this.padString(this.formatNumber(sale.totalPrice), 10)}`);
     console.log(data);
+
+    this.http.post('http://localhost:53016/api/values', {data}).subscribe();
     return data;
   }
 
