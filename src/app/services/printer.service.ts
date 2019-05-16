7dec6a3e7e8f0c7bc2655bd69d2d4b8b31e2e93d
Ji Hyeon Kim <bill.jh.kim@gmail.com>
2019-05-16 11:48:01 -0400

    wip commit

--- src/app/services/printer.service.ts
+++ src/app/services/printer.service.ts
@@ -5,7 +5,9 @@ import * as stringWidth from 'string-width';
 import { AppState } from '../core';
 import { format } from 'date-fns';
 import { HttpClient } from '@angular/common/http';
+import { BXLTextAlignment, BXLTextAttribue, BXLTextSize } from '../shared/enums/printer';
 
+/* tslint:disable */
 
 @Injectable()
 export class PrinterService {
@@ -15,42 +17,60 @@ export class PrinterService {
     private appState: AppState) { }
 
   sendPrintRequest(sale: RegisterSale) {
+    const divider = '-'.repeat(42);
     const store = this.appState.currentStore as any;
     const header1 = `${store.name} ${store.phoneNumber}`;
     const header2 = `${store.address}`;
     const header3 = `${store.representative} ${store.businessNumber}`;
-    const header4 = format(new Date(sale.salesDate), 'yyyy-MM-dd HH:mm:ss');
-    const header5 = `${this.padString(' 상 품 명', 19, false)} ${this.padString('단 가', 8)} ${this.padString('수량', 4)} ${this.padString('금 액', 8)}`;
+    const dateTime = format(new Date(sale.salesDate), 'yyyy-MM-dd HH:mm:ss');
+    const itemsHeader = `${this.format(' 상 품 명', 19, false)} ${this.format('단 가', 8)} ${this.format('수량', 4)} ${this.format('금 액', 8)}`;
+    const headerStyle = [BXLTextAlignment.Center, BXLTextAttribue.Default, BXLTextSize.W0 | BXLTextSize.H0];
+    const normalStyle = [BXLTextAlignment.Left, BXLTextAttribue.Default, BXLTextSize.W0 | BXLTextSize.H0];
+
+    // Header
     const data = [
-      header1,
-      header2,
-      header3,
-      '',
-      header4,
-      '-'.repeat(42) + '',
-      header5,
-      '-'.repeat(42) + ''];
+      [header1, ...headerStyle],
+      [header2, ...headerStyle],
+      [header3, ...headerStyle],
+      ['', ...normalStyle],
+      [dateTime, ...normalStyle],
+      [divider, ...normalStyle],
+      [itemsHeader, ...normalStyle],
+      [divider, ...normalStyle]
+    ];
+
+    // Line items
     sale.lineItems.forEach(li => {
-      const name = this.padString(li.name, 19, false);
-      const price = this.padString(this.formatNumber(li.finalPrice), 8);
-      const qty = this.padString(this.formatNumber(li.quantity), 4);
-      const total = this.padString(this.formatNumber(li.finalTotal), 8);
-      data.push(`${name} ${price} ${qty} ${total}`);
-      li.addons.forEach(ao => data.push(` ㄴ${this.padString(ao.name, 17, false)} ${this.padString(this.formatNumber(ao.price), 7)}`));
+      const name = this.format(li.name, 19, false);
+      const price = this.format(this.formatNumber(li.finalPrice), 8);
+      const qty = this.format(this.formatNumber(li.quantity), 4);
+      const total = this.format(this.formatNumber(li.finalTotal), 8);
+      data.push([`${name} ${price} ${qty} ${total}`, ...normalStyle]);
+      li.addons.forEach(ao => data.push([` ㄴ${this.format(ao.name, 17, false)} ${this.format(this.formatNumber(ao.price), 7)}`, ...normalStyle]));
     });
 
-    data.push('-'.repeat(42) + '');
-       data.push(`${this.padString('과세 물품가액', 31, false)} ${this.padString(this.formatNumber(sale.totalTaxablePrice), 10)}`);
-    data.push(`${this.padString('부가세', 31, false)} ${this.padString(this.formatNumber(sale.totalTax), 10)}`);
-    data.push(`${this.padString('합계', 31, false)} ${this.padString(this.formatNumber(sale.totalPrice), 10)}`);
+    // Totals
+    data.push([divider, ...normalStyle]);
+    data.push([`${this.format('과세 물품가액', 31, false)} ${this.format(this.formatNumber(sale.totalTaxablePrice), 10)}`, ...normalStyle]);
+    data.push([`${this.format('부가세', 31, false)} ${this.format(this.formatNumber(sale.totalTax), 10)}`, ...normalStyle]);
+    data.push([`${this.format('합계', 31, false)} ${this.format(this.formatNumber(sale.totalPrice), 10)}`, ...normalStyle]);
     console.log(data);
 
-    this.http.post('http://localhost:53016/api/values', {data}).subscribe();
+    // Customer
+    if (sale.customer) {
+      data.push([divider, ...normalStyle]);
+      data.push(['마이 포인트 내역', ...headerStyle]);
+      data.push([`${this.format('금 회 적 립 포인트', 31, false)} ${this.format(this.formatNumber(sale.pointsEarned), 10)}`, ...normalStyle]);
+      data.push([`${this.format('잔  여  포  인  트', 31, false)} ${this.format(this.formatNumber(sale.customer.currentStorePoint), 10)}`, ...normalStyle]);
+      data.push([`${this.format('사 용 가 능 포인트', 31, false)} ${this.format(this.formatNumber(sale.customer.currentStorePoint), 10)}`, ...normalStyle]);
+    }
+
+    this.http.post('http://localhost:53016/api/values', { data }).subscribe();
     return data;
   }
 
 
-  padString(value: string | number, targetLength: number, fromStart = true): string {
+  format(value: string | number, targetLength: number, fromStart = true): string {
     value = value + '';
     let str = this.subStr(value, targetLength);
     const numPad = targetLength - stringWidth(str);
---
+++ src/app/shared/enums/printer.ts
@@ -0,0 +1,33 @@
+export enum BXLTextAlignment {
+  Left, Center, Right
+}
+
+export enum BXLTextAttribue {
+  Default = 0,
+  Bold = 2,
+  Underline = 4,
+  UnderlineThick = 6,
+  Reverse = 8
+}
+
+export enum BXLTextSize {
+  W0 = 0,
+  W1 = 16,
+  W2 = 32,
+  W3 = 48,
+  H0 = 0,
+  H1 = 1,
+  H2 = 2,
+  H3 = 3,
+}
+
+export enum BXLPrinterStatus {
+  Normal = 0,
+  PaperEmpty = 1,
+  CoverOpen = 2,
+  PaperNearEnd = 4,
+  CashDrawerHigh = 16,
+  Offline = 32,
+  NotOpen = 64
+}
+
